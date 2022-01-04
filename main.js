c13beec2740dfba745d3bffc089ec5231f592feb
ashinanup <96896492+ashinanup@users.noreply.github.com>
2022-01-03 20:09:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+array_1 =["cup","paper","teddybear","ice cream"];
+var random_number= Math.floor((Math.random()*array_1.length)+1);
+console.log(array_1[random_number]);
+var sketch=array_1[random_number];
+var timer_counter=0;
+var timer_check="";
+var draw_sketch="";
+var answer_holder="";
+var score=0;
+
